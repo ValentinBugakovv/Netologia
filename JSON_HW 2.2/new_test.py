9393b3ba31fd8d6c1639dc7d455ffc1f278aeb9c
ValentinBugakovv <33461703+ValentinBugakovv@users.noreply.github.com>
2017-12-06 18:15:11 +0300

    Ver 0.2

--- JSON_HW 2.2/new_test.py
+++ JSON_HW 2.2/new_test.py
@@ -1,47 +1,49 @@
 import json
-countries_files = {
-    'africa': ['newsafr.json', 'utf8', '1'],
-    'cyprus': ['newscy.json', 'koi8-r', '1'],
-    'italy': ['newsit.json', 'cp1251', '2']
-}
+from pprint import pprint
+import chardet
 
 
-# функция для создания списка слов из всех description'ов файла ------------------------------------
-def all_descriptions_word_list(country):
-    with open(countries_files[country][0], 'r', encoding=countries_files[country][1]) as f:
-        news = json.load(f)
-        all_descriptions_word_list = []
-        for _ in news["rss"]["channel"]["item"]:
-            if countries_files[country][2] == '1':
-                all_descriptions_word_list.extend(_["description"]["__cdata"].split())
-            elif countries_files[country][2] == '2':
-                all_descriptions_word_list.extend(_["description"].split())
-    return all_descriptions_word_list
+def words_count(items): 
+    words = {}
+    for new in items:
+        new_text = new['description'].split()
+
+        for word in new_text:
+            if word in words:
+                words[word] += 1
+            elif len(word) > 6:
+                words[word] = 1
+
+    return words
 
 
-# функция для создания частотного словаря для всех слов длиннее 6 символов -------------------------
-def frequency_dictionary(all_descriptions_word_list):
-    all_descriptions_word_dict = {}
-    for word in all_descriptions_word_list:
-        if len(word) > 6 and word.isalpha():
-            if word in all_descriptions_word_dict:
-                value = all_descriptions_word_dict[word]
-                all_descriptions_word_dict[word] = value + 1
-            else:
-                all_descriptions_word_dict[word] = 1
-    return all_descriptions_word_dict
+def find_top_words(word_count): 
+
+    top_words_list = sorted(word_count.items(), key=lambda item: item[1])
+    top_words_list = list(reversed(top_words_list))
+
+    return top_words_list[:10]
 
 
-# функция для вывода топ 10 слов частотного словаря ------------------------------------------------
-def print_top_10_words(all_descriptions_word_dict):
-    count10 = sorted(set(all_descriptions_word_dict.values()), reverse=True)[9]
+def json_load(filename):
+    with open(filename, 'rb') as file:
+        data = file.read()
+        file_encoding = chardet.detect(data)
+        data = data.decode(file_encoding['encoding'])
+        data = json.loads(data)
+    items = data['rss']['channel']['items']
 
-    for word, count in sorted(all_descriptions_word_dict.items(), key=lambda x: x[1], reverse=True):
-        if count >= count10:
-            print(word, count)
-    print('------------------------------------------------')
+    return items
 
 
-for country in countries_files.keys():
-    print('топ 10 слов в файле {} и их частота:\n'.format(countries_files[country][0]))
-    print_top_10_words(frequency_dictionary(all_descriptions_word_list(country)))
+filenames = ['newsafr.json', 'newscy.json', 'newsfr.json', 'newsit.json']
+
+for filename in filenames:
+    items = json_load(filename)
+    items = words_count(items)
+
+    top_words = find_top_words(items)
+
+    print('filename %s:' % filename)
+    pprint(top_words)
+    print('\n')
