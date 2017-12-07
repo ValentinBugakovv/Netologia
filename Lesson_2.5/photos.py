4b3b59ecbd278d3095c1ab2e849a1a19aaf0563c
ValentinBugakovv <33461703+ValentinBugakovv@users.noreply.github.com>
2017-12-07 22:10:43 +0300

    new

--- Lesson_2.5/photos.py
+++ Lesson_2.5/photos.py
@@ -1,15 +1,18 @@
-from subprocess import Popen
-import shutil
 import os.path
 import glob
+from subprocess import Popen
 
 if os.path.exists("result") == False:
     result = (os.mkdir("result"))
-files_path = (os.path.abspath('/Source'))
-files = glob.glob(os.path.join(files_path, "*.jpg"))
-program_path = os.path.abspath('/convert.exe')
 
-for file in files:
-    args = [program_path, file, '-resize', '200', file]
-    shutil.copy(file, "/result")
+program_path = os.path.abspath('/convert.exe')
+source_dir = os.path.join("Source")
+result_dir = os.path.join("result")
+#files = glob.glob(os.path.join('Source', "*.jpg"))
+files_path = (os.path.abspath("Source"))
+
+for file in files_path:
+    args = ['convert.exe', source_dir, '-resize', '200', result_dir]
+    #args = ['convert.exe', file, '-resize', '200', file]
     proc = Popen(args)
+
