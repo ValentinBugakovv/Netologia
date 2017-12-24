8d4bead7e7145e2f679bc39eb1cf25edbe38ba48
ValentinBugakovv <33461703+ValentinBugakovv@users.noreply.github.com>
2017-12-24 20:50:09 +0300

    mistake correction

--- Lesson_2.4/find_procedure.py
+++ Lesson_2.4/find_procedure.py
@@ -1,16 +1,15 @@
 import os
 
-migration = os.listdir("Migrations")
-candidates = set(filter(lambda x: x.endswith('.sql'), migration))
+migrations = os.listdir("Migrations")
+candidates = filter(lambda x: x.endswith('.sql'), migrations)
 while True:
     word = input("Введите строку для поиска: ").lower()
-    candidates_copy = candidates.copy()
+    new_candidates = []
     for current_file_name in candidates:
         with open(os.path.join('Migrations', current_file_name)) as current_file:
             data = current_file.read().lower()
-            if word not in current_file_name.lower() and word not in data:
-                candidates_copy.remove(current_file_name)
-    candidates = candidates_copy.copy()
-    print(str(len(candidates)) + ' files were found')
+            if word in current_file_name.lower() or word in data:
+                new_candidates.append(current_file_name)
+    candidates = new_candidates
+    print(f"{str(len(candidates))} files were found")
     print(candidates)
-
